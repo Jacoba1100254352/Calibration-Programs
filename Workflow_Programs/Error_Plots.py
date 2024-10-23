7379f700036b18e8653165733ca356b55983b592
Jacob Anderson <jacobdanderson@gmail.com>
2024-10-22 18:57:54 -0600

    Updating with new data and results.

--- Workflow_Programs/Error_Plots.py
+++ Workflow_Programs/Error_Plots.py
@@ -15,24 +15,55 @@ plt.rc("axes", labelsize=SIZE_LARGE)  # X and Y labels fontsize
 plt.rc("axes", linewidth=2.5)  # Line width for plot borders
 
 # Data for bit resolutions and number of neurons
-neuron_counts = np.array(["Raw", 1, 2, 4, 8, 16, 32, 64, 128])  # Neuron counts, adjust as needed, bit: 12
-bit_resolutions = np.array(["Raw", 2, 4, 6, 8, 10, 12])  # Bit resolutions including "Uncalibrated", neuron: 160
+# neuron_counts = np.array(["Raw", 1, 2, 4, 8, 16, 32, 64, 128])  # Neuron counts, adjust as needed, bit: 12
+# bit_resolutions = np.array(["Raw", 2, 4, 6, 8, 10, 12])  # Bit resolutions including "Uncalibrated", neuron: 160
 
 # Originals
 # rmse_bit_resolution = np.array([0.000761, 0.160526, 0.054192, 0.014177, 0.003746, 0.001705, 0.001739])  #relu: (with tanh hyperparams) RMSE for different bit resolutions, including uncalibrated
 # rmse_neuron_count = np.array([0.000761, 0.097820, 0.065765, 0.053620, 0.010991, 0.012243, 0.006465, 0.004423, 0.005071])  # relu: (with new hyperparams (dedicated to test 9) 8-bit RMSE for different neuron counts  # activation='relu', l2_reg=0.0025, learning_rate=0.00025, epochs=100, mapping='N_vs_N', dropout_rate=0.15, layers=1, units=units, batch_size=16, bit_resolution=8
 
 # Baseline based on original data at bit resolution
-# bit_resolutions = np.array(["Raw", "B2", 2, "B4", 4, "B6", 6, "B8", 8, "B10", 10, "B12", 12])  # Bit resolutions including "Uncalibrated", neuron: 128
-# neuron_counts = np.array(["Raw", "B", 1, 2, 4, 8, 16, 32, 64, 128])  # Neuron counts, adjust as needed, bit: 12
-# rmse_neuron_count = np.array([0.000761, 0.003557, 0.003558, 0.003536, 0.003520, 0.003535, 0.003509, 0.003507, 0.003507, 0.003509]) # bit: 8
-# rmse_bit_resolution = np.array([0.000761, 0.160485, 0.160576, 0.055334, 0.054070, 0.013829, 0.013781, 0.003557, 0.003509, 0.001175, 0.000947, 0.000822, 0.000433]) # neurons: 128
+bit_resolutions = np.array(["B2", 2, "B4", 4, "B6", 6, "B8", 8, "B10", 10, "B12", 12])  # Bit resolutions including "Uncalibrated", neuron: 128
+neuron_counts = np.array(["B", 1, 2, 4, 8, 16, 32, 64, 128])  # Neuron counts, adjust as needed, bit: 12
+# rmse_neuron_count = np.array([0.003557, 0.003558, 0.003536, 0.003520, 0.003535, 0.003509, 0.003507, 0.003507, 0.003509])  # bit: 8
+rmse_neuron_count = np.array([0.000822, 0.000822, 0.000665, 0.000561, 0.000449, 0.000438, 0.000434, 0.000433, 0.000433])  # bit: 12
+rmse_bit_resolution = np.array([0.160485, 0.160576, 0.055334, 0.054070, 0.013829, 0.013781, 0.003557, 0.003509, 0.001175, 0.000947, 0.000822, 0.000433])  # neurons: 128
 
 
-# New Setup
-rmse_neuron_count = np.array([0.000761, 0.003558, 0.003536, 0.003520, 0.003535, 0.003509, 0.003507, 0.003507, 0.003509])  # bit: 8
-rmse_bit_resolution = np.array([0.000761, 0.160576, 0.054070, 0.013781, 0.003509, 0.000947, 0.000433])  # neurons: 128
+# New Setup (linear data)
+# rmse_neuron_count = np.array([0.000761, 0.003558, 0.003536, 0.003520, 0.003535, 0.003509, 0.003507, 0.003507, 0.003509])  # bit: 8
+# rmse_neuron_count = np.array([0.000761, 0.000822, 0.000665, 0.000561, 0.000449, 0.000438, 0.000434, 0.000433, 0.000433])  # bit: 12
+# rmse_bit_resolution = np.array([0.000761, 0.160576, 0.054070, 0.013781, 0.003509, 0.000947, 0.000433])  # neurons: 128
 
+# First 3: New Process
+# Compare bits with baseline
+# Compare neurons with 8 bits
+# Compare neurons with 12 bits
+
+# Next 2: Old process with unquantized baseline:
+# Compare bits
+# Compare neurons with 8 bits
+
+# Last 3: New Process with unquantized baseline
+# Compare bits with 128 neurons against unquantized baseline
+# Compare neurons with 12 bits against unquantized baseline
+# Compare neurons with 12 bits against unquantized baseline
+
+# Last Last 6:
+
+# New Setup (less linear data)
+# neuron_counts = np.array(["Raw", 1, 2, 4, 8, 16, 32, 64, 128])  # Neuron counts, adjust as needed, bit: 12
+# bit_resolutions = np.array(["Raw", 2, 4, 6, 8, 10, 12])  # Bit resolutions including "Uncalibrated", neuron: 160
+# rmse_neuron_count = np.array([0.007314, 0.007987, 0.004266, 0.003995, 0.003842, 0.003977, 0.003752, 0.003775, 0.003757])  # bit: 8
+# rmse_neuron_count = np.array([0.007314, 0.007311, 0.002580, 0.002082, 0.002073, 0.002036, 0.001378, 0.001477, 0.001449])  # bit: 12
+# rmse_bit_resolution = np.array([0.007314, 0.170761, 0.055545, 0.013788, 0.003757, 0.001692, 0.001449])  # neurons: 128
+
+# Less linear (To be filled in)
+# neuron_counts = np.array(["B", 1, 2, 4, 8, 16, 32, 64, 128])  # Neuron counts, adjust as needed, bit: 8,12
+# bit_resolutions = np.array(["B2", 2, "B4", 4, "B6", 6, "B8", 8, "B10", 10, "B12", 12])  # Bit resolutions including "Uncalibrated", neuron: 128
+# rmse_neuron_count = np.array([0.007996, 0.007987, 0.004266, 0.003995, 0.003842, 0.003977, 0.003752, 0.003775, 0.003757])  # bit: 8
+# rmse_neuron_count = np.array([0.007310, 0.007311, 0.002580, 0.002082, 0.002073, 0.002036, 0.001378, 0.001477, 0.001449])  # bit: 12
+# rmse_bit_resolution = np.array([0.170667, 0.170761, 0.056869, 0.055545, 0.015462, 0.013788, 0.007996, 0.003757, 0.007362, 0.001692, 0.007310, 0.001449])  # neurons: 128
 
 
 # Full-scale span (FSS) for FMA sensor (in Newtons)
@@ -63,7 +94,8 @@ residuals_ax.axhline(y=wearable_required_accuracy, color='b', linestyle='--',
 # residuals_ax.set_xlabel('Bit Resolution', fontsize=12)
 residuals_ax.set_ylabel(r'$\epsilon_{\text{rms}}$ (N)', fontsize=SIZE_XXXLARGE, labelpad=0)
 residuals_ax.set_yscale('log')  # Log scale for better visibility
-residuals_ax.set_ylim(1e-3, 1e0)
+# residuals_ax.set_ylim(1e-4, 1e0)
+residuals_ax.set_ylim(min(rmse_bit_resolution)*0.9, max(rmse_bit_resolution)*1.1)
 residuals_ax.grid(True)
 
 # Set bold and large font for tick labels
@@ -112,7 +144,7 @@ legend.get_frame().set_linewidth(2.0)  # Increase the outline thickness
 plt.tight_layout()
 
 file_name = "rms_bit_relu"
-plt.savefig(f"/Users/jacobanderson/Documents/BYU Classes/Current BYU Classes/Research/Papers/{file_name}.pdf", dpi=300)
+# plt.savefig(f"/Users/jacobanderson/Documents/BYU Classes/Current BYU Classes/Research/Papers/{file_name}.pdf", dpi=300)
 
 # Show plot
 plt.show()
@@ -134,7 +166,8 @@ residuals_ax.axhline(y=wearable_required_accuracy, color='b', linestyle='--',
 # residuals_ax.set_xlabel('Number of Neurons', fontsize=12)
 residuals_ax.set_ylabel(r'$\epsilon_{\text{rms}}$ (N)', fontsize=SIZE_XXXLARGE, labelpad=0)
 residuals_ax.set_yscale('log')  # Log scale for better visibility
-residuals_ax.set_ylim(1e-3, 1e0)
+# residuals_ax.set_ylim(1e-3, 1e-2)
+residuals_ax.set_ylim(min(rmse_neuron_count)*0.9, max(rmse_neuron_count)*1.1)
 residuals_ax.grid(True)
 
 # Set bold and large font for tick labels
@@ -184,7 +217,7 @@ legend.get_frame().set_linewidth(2.0)  # Increase the outline thickness
 plt.tight_layout()
 
 file_name = "rms_neuron_relu"
-plt.savefig(f"/Users/jacobanderson/Documents/BYU Classes/Current BYU Classes/Research/Papers/{file_name}.pdf", dpi=300)
+# plt.savefig(f"/Users/jacobanderson/Documents/BYU Classes/Current BYU Classes/Research/Papers/{file_name}.pdf", dpi=300)
 
 # Show plot
 plt.show()
